3b830a861d36906632f37cc325bf23beceedf865
Filteringrobika666filter <kod.github2000@gmail.com>
2024-08-10 19:40:29 +0330

    Update rubika.py
    Rubika hacking site, tutorial on hacking Rubika user accounts, free tutorial on hacking Rubika accounts

--- rubika.py
+++ rubika.py
@@ -22,3 +22,30 @@ if 'linux' in stm.lower() or 'mac' in stm.lower():
         system ('python3 hack-rubika.py')
 else:
     print('\n\033[31m[\033[36mgithun.com/mester-root/hack-rubika\033[31m]\n')
+from time import sleep
+
+
+print("###################################")
+print("|                                 |")
+print("|                                 |")
+print("|    h a c k     r u b i k a      |")
+print("|                                 |")
+print("|                                 |")
+print("|---------------------------------|")
+print("|                                 |")
+print("|   Manufacturer :    maziar      |")
+print("|                                 |")
+print("|                                 |")
+print("###################################")
+
+
+hack = input("TARGET  PHONE : ")
+
+l = ["hacking the rubika 18%...", "hacking the rubika 32%....", "hacking the rubika 58%.....", "hacking the rubika 76%......", "hacking the rubika 99%.......", " * hacking failed - Done! Crack the code  rubika target :* "]
+
+for i in l:
+    print("\r" + i, end="")
+    sleep(1.5)
+print()
+
+print("code : 6283" or "9473" or "9272")
